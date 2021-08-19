0b360276a3d4f17e34652bbe225186037215722f
Piyush Kumar <20BCS9107@cuchd.in>
2021-08-19 21:03:36 +0530

    Getting User Input in Java

---
+++ tut_5.java
@@ -0,0 +1,27 @@
+import java.util.Scanner;
+
+public class tut_5 {
+
+    public static void main(String[] args) {
+        System.out.println("Taking input from userside");
+        Scanner sc = new Scanner(System.in);
+        // System.out.println("Enter num 1");
+        // int a = sc.nextInt();
+
+        // float a = sc.nextFloat();
+        // System.out.println("Enter num 2");
+        // int b = sc.nextInt();
+        // float b = sc.nextFloat();
+        // int sum = a + b;
+        // float sum = a + b;
+        // System.out.print("Sum of num 1 and num2 is ");
+        // System.out.println(sum);
+
+        // boolean b1 = sc.hasNextInt();
+        // System.out.println(b1);
+
+        String str = sc.nextLine();
+        System.out.println(str);
+
+    }
+}

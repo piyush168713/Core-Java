b40f3452c5c10ad46ecc3d35197bffaba5d641fc
Piyush Kumar <20BCS9107@cuchd.in>
2021-08-19 21:03:07 +0530

    Basics/format of java

---
+++ Basic.java
@@ -0,0 +1,25 @@
+public class Basic {
+
+    public static void main(String[] args) {
+        // Write your code here
+        byte age = 20;
+        char ch = 'P';
+        float f1 = 5.6f;
+        short s1 = 23;
+        long l1 = 168713168713L;
+        double d1 = 5.66D;
+        System.out.println(age);
+        System.out.println(ch);
+        System.out.println(f1);
+        System.out.println(s1);
+        System.out.println(l1);
+        System.out.println(d1);
+
+        String str = "Piyush";
+        System.out.println(str);
+    }
+}
+
+// add two numbers
+// AddTwoNumbers -----> Pascal naming convention
+// addTwoNumbers -----> camel case naming convention

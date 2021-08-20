f212b05187f0879111fd88abcd1e1872e3221218
Piyush Kumar <20BCS9107@cuchd.in>
2021-08-20 16:46:33 +0530

    Operators and its Types & Expressions in Java

---
+++ tut_8.java
@@ -0,0 +1,31 @@
+
+// Operators, Types of Operators & Expressions in Java
+public class tut_8 {
+    public static void main(String[] args) {
+        // Arithmetic operator
+        int a = 6;
+        // int b = a % 4; // Modulo operator
+        // 4.8%1.1 ---> Returns decimal operator
+        // Assignment operator
+        int b = 7;
+        b *= 3;
+        System.out.println(b);
+        // Comparison operator
+        System.out.println(6 == 8);
+        System.out.println(8 <= 8);
+
+        // Logical Operator
+        System.out.println(54 > 8 && 53 > 455);
+        System.out.println(54 > 8 && 53 > 4);
+        System.out.println(54 > 8 || 53 > 4);
+        System.out.println(54 > 8 || 53 > 455);
+        System.out.println(54 > 87 || 53 > 455);
+        // Bitwise Operator
+        System.out.println(2 & 3);
+
+        // 2 --> 10
+        // 3 --> 11 Binary nos.
+        // ------------
+        // 10 using and operator
+    }
+}

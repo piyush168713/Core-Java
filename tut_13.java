136d1f991fe6ce29a22bc2bd125f0d6be96f706b
Piyush Kumar <20BCS9107@cuchd.in>
2021-08-26 14:13:25 +0530

    Introduction to Strings

---
+++ tut_13.java
@@ -0,0 +1,18 @@
+import java.util.Scanner;
+
+public class tut_13 {
+    public static void main(String[] args) {
+        // String name = new String("Piyush");
+        // String name = "Piyush";
+        // System.out.print("The name is: ");
+        // System.out.println(name);
+        int a = 6;
+        float b = 5.3455f;
+        System.out.printf("The value of a is %d and the value of b is %f.", a, b);
+        System.out.format("\nThe value of a is %d and the value of b is %8.2f.", a, b);
+        // Scanner sc = new Scanner(System.in);
+        // String st = sc.next();
+        // // String st = sc.nextLine();
+        // System.out.println(st);
+    }
+}

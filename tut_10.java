62a4130084f5138f3eb6817c766d1e8b97e42dc1
Piyush Kumar <20BCS9107@cuchd.in>
2021-08-26 14:12:48 +0530

    Data Type of Expressions & Increment/Decrement Operators

---
+++ tut_10.java
@@ -0,0 +1,22 @@
+public class tut_10 {
+    public static void main(String[] args) {
+        // byte x = 5;
+        // int y = 6;
+        // short z = 8;
+        // int a = y + z;
+        // float b = 6.54f + x;
+        // System.out.println(b);
+
+        // Increment & Decrement Operators
+        int i = 56;
+        // int b = ++i;
+        System.out.println(i++);
+        System.out.println(i);
+        System.out.println(++i);
+
+        int y = 7;
+        int x = ++y * 8;
+        System.out.println(x);
+
+    }
+}

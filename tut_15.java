81488aee0bc4accd42765d8541fe153dd7ec5d95
Piyush Kumar <20BCS9107@cuchd.in>
2021-08-27 23:01:13 +0530

    Practice Set on Java Strings

---
+++ tut_15.java
@@ -0,0 +1,31 @@
+public class tut_15 {
+    public static void main(String[] args) {
+
+        // Problem 1: Convert a string to lowercase.
+        String name = "PIYUSH";
+        System.out.println(name.toLowerCase());
+
+        // Problem 2: Replace spaces with underscore
+        String word = "Piyush learns basic java programs";
+        System.out.println(word.replace(" ", "_"));
+
+        // Problem 3:
+        // Program to fill a letter template which looks like below:
+        // letter = "Dear <|name|>, Thanks a lot"
+        // Replace <|name|> with a string (Someone)
+        String letter = "Dear <|name|>, Thanks alot";
+        System.out.println(letter.replace("<|name|>", "Piyush"));
+
+        // Problem:
+        // Program to detect double and triple spaces
+        String detect = "This is java  tutorial";
+        System.out.println(detect.indexOf("  "));
+        System.out.println(detect.indexOf("   ")); // This gives -1 because triple space do not exist in it.
+
+        // Problem 3:
+        // program to format the folloeing letter using escape sequence characters.
+        String kum = "Heyy Viewers,\n\t Me piyush Kumar.\nThanks";
+        System.out.println(kum);
+    }
+
+}

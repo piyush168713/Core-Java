98655b96d44ef9e46b4c3850f17f721e92c3d478
Piyush Kumar <20BCS9107@cuchd.in>
2021-08-28 13:34:26 +0530

    if-else statements

---
+++ tut_16.java
@@ -0,0 +1,11 @@
+
+public class tut_16 {
+    public static void main(String[] args) {
+        int age = 15;
+        if (age > 19) {
+            System.out.println("You can drive");
+        } else if (age < 19) {
+            System.out.println("You can't drive");
+        }
+    }
+}

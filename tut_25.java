c1d2839b4c918b0af3e0527ec3f0d2ddc60f8943
Piyush Kumar <20BCS9107@cuchd.in>
2021-08-31 23:13:59 +0530

    Practice Questions on Loops

---
+++ tut_25.java
@@ -0,0 +1,51 @@
+import java.util.Scanner;
+
+import javax.rmi.ssl.SslRMIClientSocketFactory;
+
+public class tut_25 {
+    public static void main(String[] args) {
+
+        // Problem 1:
+        // int n = 4;
+        // for (int i = n; i > 0; i--) {
+        // for (int j = 0; j < i; j++) {
+        // System.out.print("* ");
+        // }
+        // System.out.print("\n");
+        // }
+
+        // Problem 2:
+        // Program to sum first n even number using while loop...
+        // int sum = 0;
+        // int n = 3;
+        // for (int i = 0; i <= n; i++) {
+        // sum = sum + (2 * i);
+        // }
+        // System.out.print("The sum of even number is: ");
+        // System.out.println(sum);
+
+        // problem 3:
+        // Program to print multiplication table of a given number n......
+        // Scanner sc = new Scanner(System.in);
+        // int n;
+        // System.out.print("Enter a number: ");
+        // n = sc.nextInt();
+        // int result;
+        // // int n = 5;
+        // for (int i = 1; i <= 10; i++) {
+        // // result = i * n;
+        // // System.out.println(result);
+        // System.out.printf("%d * %d = %d\n", n, i, n * i);
+        // }
+
+        // Problem 4:
+        // Program to find factorial of a given number using for loops.....
+        int fac = 1;
+        int n = 5;
+        for (int i = 1; i <= n; i++) {
+            fac *= i;
+        }
+        System.out.println(fac);
+
+    }
+}

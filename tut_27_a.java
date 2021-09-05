0c68e02ce955d197a59834a7bfb35e665b321268
Piyush Kumar <20BCS9107@cuchd.in>
2021-09-05 13:36:42 +0530

    Loop questions in java in Classroom

---
+++ tut_27_a.java
@@ -0,0 +1,68 @@
+public class tut_27_a {
+    public static void main(String[] args) {
+
+        // ques 1:
+        // boolean b = true;
+        // if (b) {
+        // System.out.println("if block ");
+        // } else {
+        // System.out.println("else block ");
+        // }
+
+        // ques 2:
+        // for (;;) {
+        // System.out.println("infinite loop");
+        // }
+
+        // ques 3:
+        // while (false) {
+        // System.out.println("while loop");
+        // }
+
+        // ques 4:
+        // for (;;)
+        // System.out.println("For loop");
+
+        // ques 5: for-each loop
+        // int[] numbers = { 10, 20, 30, 40, 50 };
+        // for (int i : numbers) {
+        // System.out.println("i: " + i);
+        // }
+
+        // ques 6:
+        // int arr[] = { 5, 10, 15, 20, 25, 30 };
+        // int i;
+        // int search = 25;
+        // boolean b = false;
+        // for (i = 0; i < arr.length; i++) {
+        // if (arr[i] == search) {
+        // b = true;
+        // break;
+        // }
+        // }
+        // if (b) {
+        // System.out.println("Number found " + search + " at index of " + i);
+        // } else {
+        // System.out.println("Element not found");
+        // }
+
+        // ques 7:
+        // for (int i = 0; i <= 10; i++) {
+        // System.out.println(i + " ");
+        // if (i % 2 != 0)
+        // continue;
+        // System.out.println(" ");
+        // }
+
+        // ques 8:
+        // int ai[] = { 1, 2, 3, 4, 5 };
+        // int aic[] = ai;
+        // aic[2] = -9;
+        // System.out.println(aic[2]);
+        // System.out.println(ai[2]);
+        // aic = ai.clone(); // creating a new copy of ai
+        // aic[1] = -15;
+        // System.out.println(aic[1]);
+        // System.out.println(ai[1]);
+    }
+}

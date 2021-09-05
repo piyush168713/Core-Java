a40c46aa3733affa5c9c88c15a2b9d5b5a2f91df
Piyush Kumar <20BCS9107@cuchd.in>
2021-09-05 12:50:43 +0530

    For Each Loop in Java

--- tut_27.java
+++ tut_27.java
@@ -4,18 +4,21 @@ public class tut_27 {
         // System.out.println(marks.length);
         // System.out.println(marks[2]);
 
+        
         // Displaying the Array (for - loop)
         // System.out.println(" Printing using for Loop: ");
         // for (int i = 0; i < marks.length; i++) {
         // System.out.println(marks[i]);
         // }
 
+        
         // Displaying the Array (for - loop)
         // System.out.println(" Printing using for Loop in reverse order: ");
         // for (int i = marks.length - 1; i >= 0; i--) {
         // System.out.println(marks[i]);
         // }
 
+        
         // Displaying the Array (for-each loop)
         System.out.println(" Printing using for-each Loop: ");
         for (int elements : marks) {

955ab1e87e5e0523cc7242abf336d9d1d3371c62
Piyush Kumar <20BCS9107@cuchd.in>
2022-01-19 15:55:15 +0530

    Add files via upload

---
+++ Package & Interfaces/p1/Demop1.java
@@ -0,0 +1,9 @@
+package p1;
+
+public class Demop1 {
+    public static void main(String[] args) {
+        Protection obj1 = new Protection();
+        Derived obj2 = new Derived();
+        SamePackage obj3 = new SamePackage();
+    }
+}

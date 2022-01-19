9ac1b89787f65884dc65421ff90fd121bfc4ff37
Piyush Kumar <20BCS9107@cuchd.in>
2022-01-19 15:56:22 +0530

    Add files via upload

---
+++ Package & Interfaces/p2/Demop2.java
@@ -0,0 +1,8 @@
+package p2;
+
+public class Demop2 {
+    public static void main(String[] args) {
+        Otherpackage obj1 = new Otherpackage();
+        Protection2 obj2 = new Protection2();
+    }
+}

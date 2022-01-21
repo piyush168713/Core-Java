57ab423d2843334032607da759070149fea4b1dd
Piyush Kumar <20BCS9107@cuchd.in>
2022-01-21 13:32:22 +0530

    Add files via upload

---
+++ Enumerations_Autoboxing_Annotations_12/Wrap_4.java
@@ -0,0 +1,11 @@
+package Enumerations_Autoboxing_Annotations_12;
+
+public class Wrap_4 {
+    public static void main(String[] args) {
+        Integer iOb = new Integer(100);
+
+        int i = iOb.intValue();
+
+        System.out.println(i + " " + iOb);
+    }
+}

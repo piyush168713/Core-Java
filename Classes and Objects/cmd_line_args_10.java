41315d1307f74acb6f71adaa4597b15146330fa2
Piyush Kumar <20BCS9107@cuchd.in>
2022-01-25 19:01:10 +0530

    Add files via upload

---
+++ Classes and Objects/cmd_line_args_10.java
@@ -0,0 +1,12 @@
+package com.company;
+
+public class cmd_line_args_10 {
+    public static void main(String args[]) {
+        for (int i = 0; i< args.length; i++)
+            System.out.println("args[" + "]: " + args[i]);
+    }
+}
+
+
+// Try executing program
+// java com.company.cmd_line_args_10 this is a com.company.test 100 -1

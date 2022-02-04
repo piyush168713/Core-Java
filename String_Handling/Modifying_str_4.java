4c1220edd8c4ab5ae96b45d2067257d79b8379ab
Piyush Kumar <20BCS9107@cuchd.in>
2022-02-04 12:15:58 +0530

    Modifying String

---
+++ String_Handling/Modifying_str_4.java
@@ -0,0 +1,83 @@
+package String_Handling;
+
+//public class Modifying_str_4 {
+//}
+
+
+//            substring() Method
+//class StringReplace{
+//    public static void main(String[] args) {
+//        String org = "This is a test. This is, too.";
+//        String search = "is";
+//        String sub = "was";
+//        String result = " ";
+//        int i;
+//
+//        do {  // replace all matching substrings
+//            System.out.println(org);
+//            i = org.indexOf(search);
+//            if (i != -1) {
+//                result = org.substring(0, i);
+//                result = result + sub;
+//                result = result + org.substring(i + search.length());
+//                org = result;
+//            }
+//        } while (i != -1);
+//    }
+//}
+
+
+
+//                concat()  Method
+
+//class StringConcat {
+//    public static void main(String[] args) {
+//        String s1 = "one";
+//        String s2 = s1.concat("two");
+//        System.out.println(s2);
+//    }
+//}
+
+
+
+//         replace() Method
+
+//class StringReplace {
+//    public static void main(String[] args) {
+//        String s = "Hello".replace('l', 'p');
+//        System.out.println(s);
+//    }
+//}
+
+
+
+//         trim() and strip() Method
+
+class StringTrim {
+    public static void main(String[] args) {
+        String s = "   Hello World   ".trim();
+        System.out.println(s);
+    }
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
